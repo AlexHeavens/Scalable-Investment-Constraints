7ad0f436235f1518af6917fd0556b39f2eba1f3b
Alexander Heavens <alexander.heavens@gmail.com>
2018-05-26 08:12:51 +0100

    Create Abstract interface for FilterTreeFactory

---
+++ src/sic/Model/AbstractFilterTreeFactory.hh
@@ -0,0 +1,21 @@
+#ifndef SIC_ABSTRACTFILTERTREEFACTORY_H_
+#define SIC_ABSTRACTFILTERTREEFACTORY_H_
+
+#include <memory>
+
+#include "sic/Model/FilterTree.hh"
+
+namespace sic {
+
+/**
+ * Interface for objects that generate FilterTrees
+ */
+class AbstractFilterTreeFactory {
+
+public:
+	virtual std::unique_ptr<sic::FilterTree> create() = 0;
+};
+
+} // namespace sic
+
+#endif // SIC_ABSTRACTFILTERTREEFACTORY_H_
