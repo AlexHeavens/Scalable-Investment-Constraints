b69e07a3dad8feec0f766ee1580d784bf27601d7
Alexander Heavens <alexander.heavens@gmail.com>
2018-06-05 16:47:37 +0100

    Benchmark FilterTree.getLeafNode
    Using Google benchmark gives us ns timing of the individual key operations,
    helping tuning later.

---
+++ benchmark/sic/Model/FilterTreeBenchmark.cc
@@ -0,0 +1,29 @@
+#include <benchmark/benchmark.h>
+
+#include "sic/Model/RegularFilterTreeFactory.hh"
+#include "sic/Portfolio/Asset.hh"
+
+static void FilterTree_getLeafNode(benchmark::State &state) {
+
+	constexpr unsigned depth = 4;
+	constexpr unsigned nodeDegree = 4;
+	sic::RegularFilterTreeFactory factory(depth, nodeDegree);
+
+	constexpr sic::External::ID treeID = 555;
+	sic::FilterTree tree(treeID);
+	factory.create(tree);
+
+	const std::vector<unsigned> path{1, 2, 3};
+	auto classes = factory.getPathClasses(path);
+	constexpr sic::External::ID assetID = 123;
+
+	sic::Asset asset(assetID, std::move(classes));
+
+	for (auto _ : state) {
+
+		const auto &leafNode = tree.getLeafNode(asset);
+#pragma unused(leafNode)
+	}
+}
+
+BENCHMARK(FilterTree_getLeafNode);
