7f1e770c7c0e8235ab5ea68b88c88d59b10a90ec
Alexander Heavens <alexander.heavens@gmail.com>
2018-07-10 20:44:20 +0100

    Parallelise portfolio evaluation

--- use_case/sic/UseCases.hh
+++ use_case/sic/UseCases.hh
@@ -1,6 +1,8 @@
 #ifndef SIC_USECASES_H_
 #define SIC_USECASES_H_
 
+#include <thread>
+
 #include "sic/Base/Variable.hh"
 #include "sic/EvaluationContext.hh"
 
@@ -10,16 +12,43 @@ namespace UseCase {
 
 void evaluateRestrictionResults(sic::EvaluationContext &context) {
 
-	std::vector<std::string> resultStrings;
-	for (const auto &portfolio : context.getPortfolioCache()) {
-		for (const auto &aa : portfolio->getAssetAllocations()) {
-			auto results = aa->generateRestrictionResults(*portfolio);
-			for (const auto &result : *results) {
-				resultStrings.emplace_back(result->serialise());
-			}
-		}
+	int threadCount = std::thread::hardware_concurrency();
+
+	// Zero implies thread count is unknown.
+	if (threadCount == 0) {
+		threadCount = 1;
 	}
-	unused(resultStrings);
+
+	std::vector<std::thread> threads;
+
+	std::function<void(const int)> threadEvaluatePortfolio =
+		[&](const int threadID) {
+			int portfolioCount = 0;
+			std::vector<std::string> resultStrings;
+			for (const auto &portfolio : context.getPortfolioCache()) {
+
+				if (portfolioCount % threadCount == threadID) {
+					for (const auto &aa : portfolio->getAssetAllocations()) {
+						auto results =
+							aa->generateRestrictionResults(*portfolio);
+						for (const auto &result : *results) {
+							resultStrings.emplace_back(result->serialise());
+						}
+					}
+				}
+
+				portfolioCount++;
+			}
+			unused(resultStrings);
+		};
+
+	for (int threadID = 0; threadID < threadCount; threadID++) {
+		threads.emplace_back(threadEvaluatePortfolio, threadID);
+	}
+
+	for (auto &thread : threads) {
+		thread.join();
+	}
 }
 
 void filterAssets(sic::EvaluationContext &context) {
