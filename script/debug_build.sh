758d0e2b983b6d1636226342b4aa9a170e7dff45
Alexander Heavens <alexander.heavens@gmail.com>
2018-07-12 23:06:27 +0100

    Add separate debug build directly

--- script/debug_build.sh
+++ script/debug_build.sh
@@ -2,8 +2,9 @@
 
 SCRIPT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)
 PROJECT_ROOT_DIR="${SCRIPT_DIR}/.."
-BUILD_DIR="${PROJECT_ROOT_DIR}/build"
+BUILD_DIR="${PROJECT_ROOT_DIR}/build_debug"
 
+mkdir --parents ${BUILD_DIR}
 cd "${BUILD_DIR}"
-cmake .. -DCMAKE_BUILD_TYPE=Debug -DCMAKE_MAKE_PROGRAM=ninja -G Ninja
+cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_MAKE_PROGRAM=ninja -G Ninja .. 
 ninja
