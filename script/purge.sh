ce51e7f525348cad89793aaab4f5aa65f8ee14f5
Alexander Heavens <alexander.heavens@gmail.com>
2018-07-24 12:51:29 +0100

    Clean up purge script

--- script/purge.sh
+++ script/purge.sh
@@ -4,7 +4,9 @@ SCRIPT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)
 PROJECT_ROOT_DIR="$( cd "${SCRIPT_DIR}/.." && pwd)"
 
 RM_FLAGS="-fr"
-rm "$RM_FLAGS" "${PROJECT_ROOT_DIR}/bin/*" "${PROJECT_ROOT_DIR}/bin_debug/*"
-rm "$RM_FLAGS" "${PROJECT_ROOT_DIR}/build/*" "${PROJECT_ROOT_DIR}/build_debug/*"
-
-rm "$RM_FLAGS" "$PROJECT_ROOT_DIR/analysis"
+rm "$RM_FLAGS" \
+	${PROJECT_ROOT_DIR}/bin/* \
+	${PROJECT_ROOT_DIR}/bin_debug/* \
+	${PROJECT_ROOT_DIR}/build/* \
+	${PROJECT_ROOT_DIR}/build_debug/* \
+	$PROJECT_ROOT_DIR/analysis \
